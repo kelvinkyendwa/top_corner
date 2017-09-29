fee246ef7fefb06c51d036e60b1568ab986b6947
kelvinkyendwa <kelvinkyendwa@gmail.com>
2017-09-29 16:49:48 +0300

    create
    create view initial

--- topCorner/stadia/views.py
+++ topCorner/stadia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,
+from django.views.generic.edit import DeleteView,CreateView
 
 # Create your views here.
 class IndexView(TemplateView):
